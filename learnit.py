554e467c686f9f2258f077f17afe0843d25f731d
Radicis <alloyddesign@gmail.com>
2014-12-09 13:48:35 +0000

    Initial commit

---
+++ learnit.py
@@ -0,0 +1,207 @@
+import os
+import urllib
+import webapp2
+import time
+from google.appengine.api import images
+from google.appengine.ext.webapp import template
+from google.appengine.api import users
+from google.appengine.ext import db
+#from time import strftime
+
+# --- Datastore declarations --- #
+ 
+class Post(db.Model):
+	uploaded_by = db.UserProperty(required=True)		
+	title = db.StringProperty(required=True)
+	body = db.TextProperty()	 
+	tags = db.StringListProperty(required=True)
+	posted = db.DateTimeProperty(required=True, auto_now_add=True)
+	answered = db.BooleanProperty(default=False)
+	type = db.StringProperty(required=True)
+
+class LearnUsers(db.Model):
+	pass
+
+# --- End Datastore Declarations --- #
+
+
+class MainHandler(webapp2.RequestHandler):
+	def get(self):		
+		
+		user = users.get_current_user()		
+		
+		posts_title = 'Latest'		
+		
+		html = template.render('templates/index.html', {'user': user, 'logout_url': users.create_logout_url('/')})	
+			
+		posts = Post.all().fetch(1000)	
+		#query().order
+
+		html += template.render('templates/posts.html', {'posts_title': posts_title, 'posts':posts})
+		html += template.render('templates/footer.html', {})
+		self.response.write(html)
+#		else:
+			#self.redirect(users.create_login_url(self.request.uri))
+	def post(self):
+		user = users.get_current_user()
+		searchTerm = self.request.get('search')
+		
+		posts_title = "Search Results"
+		
+		posts = Post.all().filter('tags =', searchTerm).fetch(1000)
+		
+		html = template.render('templates/index.html', {'user': user, 'logout_url': users.create_logout_url('/')})
+		
+		html += template.render('templates/posts.html', {'posts_title': posts_title, 'posts':posts})
+		
+		html += template.render('templates/footer.html', {})
+		
+		self.response.write(html)
+			
+class WritePost(webapp2.RequestHandler):
+	def get(self):
+		
+		type = self.request.get('type')
+		
+		user = users.get_current_user()
+		
+		if user:
+			html = template.render('templates/index.html', {'user': user, 'logout_url': users.create_logout_url('/')})
+			if type == 'link':
+				html += template.render('templates/make-link.html', {})
+			elif type == 'post':
+				html += template.render('templates/make-post.html', {})
+			elif type == 'question':
+				html += template.render('templates/make-question.html', {})
+			html += template.render('templates/footer.html', {})
+			self.response.write(html)			
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
+		
+class MakePost(webapp2.RequestHandler):
+	def post(self):
+		
+		type = self.request.get('type')		
+		
+		post_title = self.request.get('title',default_value='') 
+		post_body = self.request.get('body',default_value='') 
+		post_tags = self.request.get('tags',default_value='').split()	
+
+		post_info = Post(type=type, title=post_title, body=post_body, tags=post_tags, uploaded_by=users.get_current_user())	
+		
+		db.put(post_info)
+		
+		time.sleep(3) # wait for 3s to allow for entry to be added so it displays on redirect
+		
+		self.redirect('/')
+
+class ViewPost(webapp2.RequestHandler):
+	def get(self):
+		post_id = long(self.request.get('post'))
+
+		posts = Post.all()
+
+		post = Post.get_by_id(post_id)
+		user = users.get_current_user()
+		
+		html = template.render('templates/index.html', {'user': user, 'logout_url': users.create_logout_url('/')})
+		html += template.render('templates/view.html', {'post':post})
+		html += template.render('templates/footer.html', {})
+		self.response.write(html)
+		
+class Unanswered(webapp2.RequestHandler):
+	def get(self):
+		user = users.get_current_user()		
+		
+		posts_title = 'Unanswered'		
+		post_type = 'question'
+		
+		html = template.render('templates/index.html', {'user': user, 'logout_url': users.create_logout_url('/')})	
+		#watch the space before = it is needed
+		posts = Post.all().filter('type =', post_type).filter('answered =', False).fetch(10000)
+		
+		html += template.render('templates/posts.html', {'posts_title': posts_title, 'posts':posts})
+		
+		html += template.render('templates/footer.html', {})
+		
+		self.response.write(html)
+
+class ViewTags(webapp2.RequestHandler):
+	def get(self):
+		user = users.get_current_user()
+		posts_title = "Tags"
+		
+		html = template.render('templates/index.html', {'user': user, 'logout_url': users.create_logout_url('/')})
+		
+		posts = Post.all().fetch(1000)
+		
+		tags = []
+		
+		for post in posts:
+			for tag in post.tags:
+				if tag not in tags:
+					tags.append(tag)		
+		
+		html += template.render('templates/tags.html', {'tags':tags})
+		
+		html += template.render('templates/footer.html', {})
+		self.response.write(html)
+
+
+class ViewTag(webapp2.RequestHandler):
+	def get(self):
+		
+		user = users.get_current_user()
+		tag = self.request.get('tag')
+		
+		posts_title = tag
+		
+		html = template.render('templates/index.html', {'user': user, 'logout_url': users.create_logout_url('/')})
+			
+		posts = Post.all().filter('tags = ', tag)
+		posts.fetch(10)
+		
+		html += template.render('templates/posts.html', {'posts_title': posts_title, 'posts':posts})
+		
+		html += template.render('templates/footer.html', {})
+		
+		self.response.write(html)
+
+		
+class Search(webapp2.RequestHandler):
+	def post(self):
+		
+		searchTerm = self.request.get('search')
+		
+		posts = Post.all().filter('tags =#', searchTerm).fetch(1000)
+		
+		html = template.render('templates/index.html', {'user': user, 'logout_url': users.create_logout_url('/')})
+		
+		html += template.render('templates/posts.html', {'posts_title': posts_title, 'posts':posts})
+		
+		html += template.render('templates/footer.html', {})
+		
+		self.response.write(html)
+		
+	
+		
+class DeletePost(webapp2.RequestHandler):
+	def post(self):
+		pass
+		
+class About(webapp2.RequestHandler):
+	def get(self):
+		pass
+		
+app = webapp2.WSGIApplication([('/', MainHandler),
+                               ('/post', WritePost),
+							   ('/makepost', MakePost),
+							   ('/view', ViewPost),
+							   ('/delete', DeletePost),
+							   ('/unanswered', Unanswered),
+							   ('/tags', ViewTags),	
+							   ('/tag', ViewTag),
+							   ('/about', About),							   
+							   ],
+                              debug=True)
+
