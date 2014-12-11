b8e706139f94a0764087c91cd6c382b14376d45c
Radicis <alloyddesign@gmail.com>
2014-12-11 20:21:18 +0000

    revised color scheme

--- static/js/main.js
+++ static/js/main.js
@@ -2,11 +2,16 @@ $(document).ready(function(){
 	$("#makepost").click(function(){
 	  $("#right-make-post-inner").slideToggle();
 	}); 
+	$(".logo").click(function(){
+	  $("#main-nav").slideToggle();
+	}); 
+	
 	$('.posts-container').profanityFilter({
     replaceWith: ['fun', 'stuff'],
     customSwears: ['ass'],
     externalSwears: '/static/js/vendor/swearWords.json'
 	});
+	
 	$('.side-content').profanityFilter({
     replaceWith: ['fun', 'stuff'],
     customSwears: ['ass'],
